2f94366245ae8c24ce87c481de869f3de024e338
Sjoholm <Sjoholm@users.noreply.github.com>
2023-09-16 12:13:24 +0000

    Copilot first commit

--- skills.js
+++ skills.js
@@ -1,13 +1,3 @@
-function calculateNumber(var1, var2) {
+function calculateNumbers(var1, var2) {
   return Math.round(var1) + Math.round(var2);
-}
-
-function testCalculateNumber() {
-  const a = 1;
-  const b = 2;
-  console.log(calculateNumber(a, b));
-}
-
-function returnRandomNumberBetween1and100() {
-  return Math.floor(Math.random() * 100) + 1;
 }
